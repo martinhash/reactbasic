992d61ed1aa93f34e4a0442fbaa7806bcded6a46
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-03 16:25:34 -0600

    fix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import React from 'react';
 import './App.css';
 
 
-function Helloworld(props){
-  return (
-    <div id="helloworld">
-      <h3>{props.subtitle}</h3>
-     {props.mytext}
-    </div>
-  );
+// function Helloworld(props){
+//   return (
+//     <div id="helloworld">
+//       <h3>{props.subtitle}</h3>
+//      {props.mytext}
+//     </div>
+//   );
+// }
+
+class Helloworld extends React.Component{
+
+
+  state = {
+    show: true
+  }
+  changeState = () => {
+   this.setState({show:!this.state.show})
+  }
+
+  render(){
+    if(this.state.show){
+      return (
+        <div id="hello">
+          <h3>{this.props.mytext}</h3>
+          {this.props.subtitle}
+          <button onClick={this.changeState}>Turn Off</button>
+        </div>
+      )
+    }else{
+      return <div>
+        <h1>No existe nada</h1>
+        <button onClick={this.changeState}>Turn On</button>
+      </div> 
+    }
+  }
 }
 
 function App() {
